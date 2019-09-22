774e71fdd13f087ccf03fa611f2862d2fde5187b
NetHunter98 <55172998+NetHunter98@users.noreply.github.com>
2019-09-22 13:36:17 +0800

    Add files via upload

---
+++ Install-RS.sh
@@ -0,0 +1,52 @@
+#!/system/bin/sh
+
+clear
+
+
+echo "━━━╮        " 
+echo "━━╮╰╮┊┊┊┊┊┊"
+echo "┊┊╰╮╰━▂▂▂▂┊┊┊┊┊┊ "
+echo "┊▂╱▔╲▔╱┏┳╮╲┊┊ᶤ.╭╮ "
+echo "▂╲▂▂╱╲╲╰┻┛╱▔▔▔▔┃ " 
+echo "╲▂▂╱╭╯╱▔▔╱▔▔▔▽▽╯ "
+echo "┊╱╱╭╯╱▔▔▔╲▂▂△▂△╮ "
+echo "━━━╯╱╱╭━━━━━━━━╯  "
+echo
+
+echo "#####***_-NetHunter-_@@@#####" 
+echo
+echo "===================[~]===================="
+echo
+read -p "apakah kamu tetap ingin melanjutkan ? y/n : " aa
+echo
+if [ $aa = "y" ]
+then
+    sleep 1
+
+    echo "_________________________________"
+    echo
+    echo "# sedang manginstall RapidScan..."
+    echo "_________________________________"
+    echo
+    sleep 10
+    $HOME
+    apt install wget
+    wget -O rapidscan.py https://raw.githubusercontent.com/skavngr/rapidscan/master/rapidscan.py && chmod +x rapidscan.py
+
+    figlet "Terinstall"
+    echo
+    sleep 3
+    echo "# RapidScan berhasil diinstall "
+    echo "# cek di HOME termuxMu"
+    echo
+    
+    fi
+
+if [ $aa = "n" ]
+then
+    sleep 1
+    clear
+    exit
+    echo
+    
+    fi
