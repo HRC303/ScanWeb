92c0f5efe6260d68976331316cbba765eb61c5cd
MR.XXII <betazona1984@gmail.com>
2021-07-16 05:55:57 +0900

    Update ScanWEBXXII.sh

--- ScanWEBXXII.sh
+++ ScanWEBXXII.sh
@@ -31,7 +31,7 @@ then
     sleep 10
     $HOME
     apt install wget
-    wget -O ZonaredXXII.py https://github.com/zonaredXXII/HackWebXXII.git && chmod +x ZonaredXXII.py
+    wget -O ZonaredXXII.py https://github.com/zonaredXXII/ZonaredXXII.git && chmod +x ZonaredXXII.py
 
     figlet "Terinstall"
     echo
